4071ea582e157a1732fc3b2ee4242cc1c8a13059
cutymeoRBLX <hkhanhnam81@gmail.com>
2024-04-04 00:10:51 +0700

    h

--- src/commands/lover.ts
+++ src/commands/lover.ts
@@ -2,7 +2,13 @@ import { Command } from '@sapphire/framework';
 
 export class PingCommand extends Command {
   public constructor(context: Command.LoaderContext, options: Command.Options) {
-    super(context, { ...options });
+    super(context, {
+        ...options,
+        aliases: [
+            'i love u, please be my lover',
+            'I love u Shiroko-chan, please be my lover'
+        ],
+    });
   }
 
   public override registerApplicationCommands(registry: Command.Registry) {
